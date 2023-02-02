a8f4321b9faf1fc3986286d28f073559fc5be965
Manit Gandhi <tripleking123@gmail.com>
2023-02-02 22:30:10 +0700

    finish tests for all apis

--- test_vending.py
+++ test_vending.py
@@ -12,11 +12,13 @@ def test_can_add_vending_machine():
     assert add_vending_response.status_code == 200
 
     data = add_vending_response.json()
-    print(data)
 
     machine_id = data['_id']['$oid']
     get_vending_response = get_vending(machine_id)
     assert get_vending_response.status_code == 200
+
+    #Delete the vending machine so the database will not be affected
+    delete_vending(machine_id)
 
 def test_can_update_vending():
     #add a vending machine
@@ -34,7 +36,9 @@ def test_can_update_vending():
     }
     update_vending_response = update_vending(new_payload)
     assert update_vending_response.status_code == 200
-    
+
+   #Delete the vending machine so the database will not be affected
+    delete_vending(machine_id) 
 
 def test_can_show_vending():
     #Add a vending machines
@@ -45,6 +49,9 @@ def test_can_show_vending():
     
     show_vending_response = get_vending(machine_id)
     assert show_vending_response.status_code == 200
+
+    #Delete the vending machine so the database will not be affected
+    delete_vending(machine_id)
 
 def test_can_delete_vending():
    #Add a vending machine
@@ -57,7 +64,53 @@ def test_can_delete_vending():
     show_vending_response = delete_vending(machine_id)
     assert show_vending_response.status_code == 200 
 
+def test_can_show_stock():
+   #Add a vending machine
+    payload = new_vending_payload()
+    add_vending_response = create_vending(payload)
+    assert add_vending_response.status_code == 200
+    machine_id = add_vending_response.json()['_id']['$oid']
+    
+    #Show stock
+    show_stock_response = get_stock(machine_id)
+    assert show_stock_response.status_code == 200
 
+    #Delete the vending machine so the database will not be affected
+    delete_vending(machine_id)
+
+def test_can_update_stock():
+   #Add a vending machine
+    payload = new_vending_payload()
+    add_vending_response = create_vending(payload)
+    assert add_vending_response.status_code == 200
+    machine_id = add_vending_response.json()['_id']['$oid']
+    
+    #Show stock
+    stock_payload = new_stock_payload(machine_id)
+    update_stock_response = update_stock(stock_payload)
+    assert update_stock_response.status_code == 200
+
+    #Delete the vending machine so the database will not be affected
+    delete_vending(machine_id)
+
+def test_can_delete_stock():
+   #Add a vending machine
+    payload = new_vending_payload()
+    add_vending_response = create_vending(payload)
+    assert add_vending_response.status_code == 200
+    machine_id = add_vending_response.json()['_id']['$oid']
+    
+    #Show stock
+    stock_payload = new_stock_payload(machine_id)
+    update_stock_response = update_stock(stock_payload)
+    assert update_stock_response.status_code == 200
+    item_name = 'Coke'
+    #Delete stock
+    delete_payload = get_delete_payload(machine_id,item_name)
+    delete_stock_response = delete_stock(delete_payload)
+    assert delete_stock_response.status_code == 200
+    #Delete the vending machine so the database will not be affected
+    delete_vending(machine_id)  
 
 
 def create_vending(payload):
@@ -73,6 +126,15 @@ def delete_vending(machine_id):
     payload = {"oid": machine_id}
     return requests.post(ENDPOINT +'deletevending/', json=payload)  
 
+def get_stock(machine_id):
+    return requests.get(ENDPOINT + f"showstock/{machine_id}") 
+
+def update_stock(payload):
+    return requests.post(ENDPOINT + f"updatestock/", json=payload)   
+
+def delete_stock(payload):
+    return requests.post(ENDPOINT + f"deletestock/", json=payload) 
+
 def new_vending_payload():
     return {"name": "Cold Drinks Test",
     "location": "Old building 34th Floor Test",
@@ -87,3 +149,19 @@ def new_vending_payload():
         "Yakult": 12,
         "Meiji Milk (Chocolate)": 11
     }} 
+
+def new_stock_payload(machine_id):
+    return {
+    "oid": machine_id,
+    "stock": {
+        "Coke": 1,
+        "Sprite": 2,
+        "Milshake": 3
+    }
+}
+
+def get_delete_payload(machine_id, item_name):
+    return {
+    "oid": machine_id,
+    "item_name":item_name
+}
